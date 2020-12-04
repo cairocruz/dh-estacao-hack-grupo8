a12e056107c9dd737b652ecadfa0ebad86bed4c7
Daniel Guimarães <44902038+danguimaraes86@users.noreply.github.com>
2020-12-04 20:51:23 -0300

    add: suporte pra múltiplos filmes

--- resultados/js/showResults.js
+++ resultados/js/showResults.js
@@ -1,19 +1,45 @@
 window.onload = createFilmeCard
 
+const filmesExample = [
+  {
+    poster: '../infoCompleta/index.html',
+    title: 'The Hill (2020?)'
+  },
+  {
+    poster: '../infoCompleta/index.html',
+    title: 'The Hill (2020?)'
+  },
+  {
+    poster: '../infoCompleta/index.html',
+    title: 'The Hill (2020?)'
+  },
+  {
+    poster: '../infoCompleta/index.html',
+    title: 'The Hill (2020?)'
+  },
+  {
+    poster: '../infoCompleta/index.html',
+    title: 'The Hill (2020?)'
+  }
+]
+
 function createFilmeCard() {
   const showListView = document.querySelector('#showListResults')
-  const card = document.createElement('div')
-  card.innerHTML = createPoster()
-  showListView.appendChild(card)
+  filmesExample.forEach((filme, i) => {
+    const card = document.createElement('div')
+    card.classList.add('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3', 'mb-4')
+    card.innerHTML = createPoster(i)
+    showListView.appendChild(card)
+  });
 }
  
-function createPoster() {
+function createPoster(i) {
   return `
-    <a href = "../infoCompleta/index.html">
+    <a href = ${filmesExample[i].poster}>
       <img src="./img/poster-the-hill.jpg" width="200" alt="" />
     </a >
     <h4>
-      The Hill (2020?)
+      ${filmesExample[i].title}
     </h4>
   `
 }
